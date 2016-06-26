dbf6749cfd927df861a400f1337ddab3d1d663a0
tiagomartinho <tiago.martinho@xpeppers.com>
2016-06-26 23:18:23 +0200

    defines pclLibraryTests to test common shared code

---
+++ pclLibraryTests/MyClassTests.cs
@@ -0,0 +1,16 @@
+﻿using System;  
+using System.Threading.Tasks;  
+using Xunit;
+using pclLibrary;
+
+namespace pclLibraryTests
+{
+	public class MyClassTests
+	{
+		[Fact]
+		public void GenerateShouldAddOldWithNew()
+		{
+			Assert.Equal("oldnew", pclLibrary.MyClass.Generate("old", "new"));
+		}
+	}
+}
